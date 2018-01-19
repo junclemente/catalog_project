b2a0e5303b9c5d589ee65229495dbb2e60e46dd8
Jun Clemente <kjstudios858@gmail.com>
2018-01-19 20:51:43 +0000

    docs Added docstring

--- create_db.py
+++ create_db.py
@@ -1,3 +1,7 @@
+"""
+This file is used to initialize a database for this project.
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -6,17 +10,26 @@ from models import User, Category, Item, Base
 
 
 engine = create_engine('sqlite:///catalogProject.db')
+# Bind the engine to the metadata of the Base class so that decleratives can
+# be accessed through a DBSession instance
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
+# DBSession() instance establishes all converstations with the DB and
+# represents a staging zone for all ojbects loaded into the DB session object.
+# Any change made against the objects in the session won't be persisted into
+# the DB until it is commited by calling: session.commit(). Changes can be
+# reverted to the last commit by calling: session.rollback()
 session = DBSession()
 
 
+# Create 'master' user.
 user1 = User(username="master")
 session.add(user1)
 session.commit()
 print "Created user: 'master', password: '12345'"
 
 
+# Create initial category
 category1 = Category(name="Snowboard",
                      user_id="1",
                      description="Snowboarding is a recreational activity...")
@@ -24,6 +37,7 @@ session.add(category1)
 session.commit()
 
 
+# Create initial item
 item1 = Item(name="Flow Merc",
              category_id="1",
              user_id="1",
