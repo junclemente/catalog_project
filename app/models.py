c76072a5ca6a2119f65840853a1bbcabfcf83076
Jun C <kjstudios858@gmail.com>
2018-01-19 23:23:37 -0800

    refactor Reorganized to create modules

--- app/models.py
+++ app/models.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import(TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+
 
 Base = declarative_base()
 
@@ -21,9 +23,9 @@ class User(Base):
     def verify_password(self, password):
         return pwd_context.verify(password, self.password_hash)
 
-    # def generate_auth_token(self, expiration=600):
-    #     s = Serializer(secret_key, expires_in=expiration)
-    #     return s.dumps({'id': self.id})
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
 
     @staticmethod
     def verify_auth_token(token):
--- application.py
+++ application.py
@@ -1,56 +1,9 @@
 #!/usr/bin/env python
+from app import app
+# from flask import flask
 
-from flask import Flask, url_for, render_template, request
-from flask import jsonify
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base, User, Category, Item
-
-
-app = Flask(__name__)
-
-
-engine = create_engine('sqlite:///catalogProject.db')
-Base.metadata.bind = engine
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    # return "<h1>Hello World!</h1>"
-    return render_template('index.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        print username, password
-
-    return render_template('login.html')
-
-
-
-@app.route('/categoryJSON')
-@app.route('/categoryjson')
-def categoryJSON():
-    categories = session.query(Category).all()
-    return jsonify(categories = [c.serialize for c in categories])
-
-
-@app.route('/itemJSON/<int:id>')
-@app.route('/itemjson/<int:id>')
-def itemJSON(id):
-    items = session.query(Item).filter_by(category_id=id).all()
-    return jsonify(items = [i.serialize for i in items])
-
+# app = Flask(__name__)
 
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=8000)
-
-
