6a9c57ca94608b84ff30f6cf7298b38dd4bb0775
Jun Clemente <kjstudios858@gmail.com>
2018-01-21 07:04:37 +0000

    refactor Add html pages to show lists

--- app/views.py
+++ app/views.py
@@ -19,3 +19,11 @@ def index():
     category = session.query(Category).all()
     items = session.query(Item).order_by(Item.id.desc()).all()
     return render_template('index.html', category=category, items=items)
+
+
+@app.route('/category_list/<int:cat_id>')
+def category_list(cat_id):
+    category = session.query(Category).filter_by(id=cat_id).one()
+    items = session.query(Item).filter_by(category_id=cat_id).all()
+    return render_template('category_list.html', category=category,
+                           items=items)
