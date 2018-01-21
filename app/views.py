e3ccda10159cd8d0840c6c7378971402a275e447
Jun C <kjstudios858@gmail.com>
2018-01-20 21:45:48 -0800

    refactor Add item list to /index page

--- app/views.py
+++ app/views.py
@@ -2,8 +2,20 @@ from app import app
 
 from flask import render_template
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models import Base, Category, Item
+
+
+engine = create_engine('sqlite:///catalogProject.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    category = session.query(Category).all()
+    items = session.query(Item).order_by(Item.id.desc()).all()
+    return render_template('index.html', category=category, items=items)
