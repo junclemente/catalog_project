dd45cdad9e5577febbf9cbe74c507d0ff731441a
Jun Clemente <kjstudios858@gmail.com>
2018-01-22 17:10:16 +0000

    refactor Add ItemForm

--- app/forms.py
+++ app/forms.py
@@ -13,3 +13,16 @@ class CategoryForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired()])
     description = TextAreaField('Description')
     submit = SubmitField('Add Category')
+
+
+class ItemForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    maker = StringField('Make', validators=[DataRequired()])
+    model_year = StringField('Year')
+    description = TextAreaField('Description')
+    price = StringField('Price')
+    submit = SubmitField('Add')
+
+
+class ConfirmForm(FlaskForm):
+    submit = SubmitField('Confirm')
