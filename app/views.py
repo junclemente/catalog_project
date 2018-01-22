650eb117188faff3e4ba6e6e5d7cdfa05cece6cd
Jun Clemente <kjstudios858@gmail.com>
2018-01-22 17:14:27 +0000

    refactor Added new form pages and code for adding item and for deleting category

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from app import app
 
 from flask import render_template, flash, redirect, url_for
 
-from forms import CategoryForm
+from forms import CategoryForm, ConfirmForm, ItemForm
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -50,3 +50,33 @@ def add_category():
         flash('New Category Added')
         return redirect(url_for('index'))
     return render_template('add_category.html', form=form)
+
+
+@app.route('/delete_category/<int:cat_id>', methods=['GET', 'POST'])
+def delete_category(cat_id):
+    form = ConfirmForm()
+    category = session.query(Category).filter_by(id=cat_id).first()
+    if form.validate_on_submit():
+        session.delete(category)
+        session.commit()
+        flash("Category deleted.")
+        return redirect(url_for('index'))
+    return render_template('delete_category.html',
+                           category=category,
+                           form=form)
+
+
+@app.route('/add_item/<int:cat_id>', methods=['GET', 'POST'])
+def add_item(cat_id):
+    form = ItemForm()
+    if form.validate_on_submit():
+        new_item = Item(name=form.name.data,
+                        # maker=form.maker.data,
+                        # model_year=form.model_year.data,
+                        description=form.description.data,
+                        category_id=cat_id)
+        session.add(new_item)
+        session.commit()
+        flash('Item added successfully.')
+        return redirect(url_for('index'))
+    return render_template('add_item.html', cat_id=cat_id,form=form)
