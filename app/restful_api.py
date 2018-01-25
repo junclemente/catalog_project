2378d284c7933b1e395a05883e921ab4d631b8ce
Jun Clemente <kjstudios858@gmail.com>
2018-01-25 18:55:18 +0000

    refactor Change routes to be more descriptive

--- app/restful_api.py
+++ app/restful_api.py
@@ -13,29 +13,25 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-@app.route('/categoryJSON')
-@app.route('/categoryjson')
+@app.route('/api/allcategories')
 def categoryJSON():
     categories = session.query(Category).all()
     return jsonify(categories = [c.serialize for c in categories])
 
 
-@app.route('/itemsInCatJSON/<int:cat_id>')
-@app.route('/itemsincatjson/<int:cat_id>')
+@app.route('/api/itemsincategory/<int:cat_id>')
 def itemsInCatJSON(cat_id):
     items = session.query(Item).filter_by(category_id=cat_id).all()
     return jsonify(items = [i.serialize for i in items])
 
 
-@app.route('/allItemsJSON')
-@app.route('/allitemsjson')
+@app.route('/api/allitems')
 def allItemsJSON():
     items = session.query(Item).all()
     return jsonify(items = [i.serialize for i in items])
 
 
-@app.route('/itemJSON/<int:id>')
-@app.route('/itemjson/<int:id>')
+@app.route('/api/item/<int:id>')
 def itemJSON(id):
     items = session.query(Item).filter_by(category_id=id).all()
     return jsonify(items = [i.serialize for i in items])
